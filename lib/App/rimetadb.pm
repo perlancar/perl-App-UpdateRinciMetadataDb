38a0b28d128d01546736c571d7df04d9cefa3ca3
perlancar (@netbook-zenbook) <perlancar@gmail.com>
2015-12-01 17:23:22 +0700

    Finish first version of rimetadb

--- lib/App/rimetadb.pm
+++ lib/App/rimetadb.pm
@@ -9,31 +9,7 @@ use warnings;
 use experimental 'smartmatch';
 use Log::Any::IfLOG '$log';
 
-use Data::Clean::JSON;
-use DBI;
-use JSON;
 use Module::Load qw(autoload load);
-use Module::Path::More;
-use Perinci::Access::Perl;
-use SQL::Schema::Versioned;
-
-use Data::Clean::JSON;
-
-my $cleanser = Data::Clean::JSON->get_cleanser;
-
-our %SPEC;
-
-sub _is_excluded {
-    my ($x, $exc_list) = @_;
-    for (@$exc_list) {
-        if (/(.+)::\*?\z/) {
-            return 1 if index($x, "$1\::") == 0;
-        } else {
-            return 1 if $x eq $_;
-        }
-    }
-    0;
-}
 
 our $db_schema_spec = {
     latest_v => 3,
@@ -68,29 +44,106 @@ our $db_schema_spec = {
     ],
 };
 
-$SPEC{update_rinci_metadata_db} = {
+sub _cleanser {
+    require Data::Clean::JSON;
+    state $cleanser = Data::Clean::JSON->get_cleanser;
+    $cleanser;
+}
+
+sub _json {
+    require JSON;
+    state $json = JSON->new->allow_nonref;
+    $json;
+}
+
+sub _pa {
+    require Perinci::Access::Perl;
+    state $pa = Perinci::Access::Perl->new;
+    $pa;
+}
+
+sub _connect_db {
+    require DBI;
+    require SQL::Schema::Versioned;
+
+    my %args = @_;
+
+    my $dbh = DBI->connect($args{dsn}, $args{user}, $args{password},
+                           {RaiseError=>1});
+
+    my $res = SQL::Schema::Versioned::create_or_update_db_schema(
+        spec => $db_schema_spec,
+        dbh => $dbh,
+    );
+    return $res unless $res->[0] == 200;
+    ($res, $dbh);
+}
+
+our %SPEC;
+
+$SPEC{':package'} = {
     v => 1.1,
-    summary => 'Create/update Rinci API metadata database',
-    args => {
-        dsn => {
-            summary => 'DBI connection DSN',
-            description => <<'_',
+    summary => 'Manage a Rinci metadata database',
+};
+
+our %args_common = (
+    dsn => {
+        summary => 'DBI connection DSN',
+        description => <<'_',
 
 Note: has been tested with MySQL and SQLite only.
 
 _
-            schema => 'str*',
-            req => 1,
-            pos => 0,
-        },
-        user => {
-            summary => 'DBI connection user',
-            schema => 'str*',
-        },
-        password => {
-            summary => 'DBI connection password',
-            schema => 'str*',
-        },
+        schema => 'str*',
+        req => 1,
+        tags => ['common'],
+    },
+    user => {
+        summary => 'DBI connection user',
+        schema => 'str*',
+        tags => ['common'],
+    },
+    password => {
+        summary => 'DBI connection password',
+        schema => 'str*',
+        tags => ['common'],
+    },
+);
+
+our %args_query = (
+    query => {
+        schema => 'str*',
+        pos => 0,
+    },
+    detail => {
+        schema => 'bool',
+        cmdline_aliases => {l=>{}},
+    },
+);
+
+sub _is_excluded {
+    my ($x, $exc_list) = @_;
+    for (@$exc_list) {
+        if (/(.+)::\*?\z/) {
+            return 1 if index($x, "$1\::") == 0;
+        } else {
+            return 1 if $x eq $_;
+        }
+    }
+    0;
+}
+
+$SPEC{update_from_modules} = {
+    v => 1.1,
+    summary => 'Update Rinci metadata database from local Perl modules',
+    description => <<'_',
+
+This routine scans Perl modules, load them, and update the database using Rinci
+metadata from each modules into the database.
+
+_
+    args => {
+        %args_common,
         module_or_package => {
             summary => 'Perl module or prefixes or package to add/update',
             description => <<'_',
@@ -115,12 +168,14 @@ For each entry, you can specify:
 _
             schema => ['array*' => of => 'str*'],
             req => 1,
-            pos => 1,
+            pos => 0,
             greedy => 1,
+            'x.schema.element_entity' => 'modulename',
         },
         exclude => {
             summary => 'Perl package names or prefixes to exclude',
             schema => ['array*' => of => 'str*'],
+            'x.schema.element_entity' => 'modulename',
         },
         library => {
             summary => "Include library path, like Perl's -I",
@@ -138,6 +193,7 @@ _
                 require lib;
                 lib->import($args{value});
             },
+            'x.schema.element_entity' => 'modulename',
         },
         use => {
             schema => ['array' => of => 'str*'],
@@ -155,6 +211,7 @@ _
                     autoload $val;
                 }
             },
+            'x.schema.element_entity' => 'modulename',
         },
         require => {
             schema => ['array' => of => 'str*'],
@@ -166,6 +223,7 @@ _
                 $log->debug("Loading module $val ...");
                 load $val;
             },
+            'x.schema.element_entity' => 'modulename',
         },
         force_update => {
             summary => "Force update database even though module ".
@@ -185,26 +243,14 @@ _
         dry_run => 1,
     },
 };
-sub update_rinci_metadata_db {
+sub update_from_modules {
+    require Module::List;
+    require Module::Path::More;
+    require Package::MoreUtil;
+
     my %args = @_;
 
-    require DBI;
-    require JSON;
-    require Module::List;
-    require Package::MoreUtil;
-    require Perinci::Access::Perl;
-    require SQL::Schema::Versioned;
-
-    state $json = JSON->new->allow_nonref;
-    state $pa = Perinci::Access::Perl->new;
-
-    my $dbh = DBI->connect($args{dsn}, $args{user}, $args{password},
-                           {RaiseError=>1});
-
-    my $res = SQL::Schema::Versioned::create_or_update_db_schema(
-        spec => $db_schema_spec,
-        dbh => $dbh,
-    );
+    my ($res, $dbh) = _connect_db(%args);
     return $res unless $res->[0] == 200;
 
     my $exc = $args{exclude} // [];
@@ -266,15 +312,15 @@ sub update_rinci_metadata_db {
 
         my $uri = $pkg; $uri =~ s!::!/!g; $uri = "pl:/$uri/";
 
-        $res = $pa->request(meta => "$uri");
+        $res = _pa->request(meta => "$uri");
         die "Can't meta $uri: $res->[0] - $res->[1]" unless $res->[0] == 200;
-        $cleanser->clean_in_place(my $pkgmeta = $res->[2]);
+        _cleanser->clean_in_place(my $pkgmeta = $res->[2]);
 
-        $res = $pa->request(list => $uri, {type=>'function'});
+        $res = _pa->request(list => $uri, {type=>'function'});
         die "Can't list $uri: $res->[0] - $res->[1]" unless $res->[0] == 200;
         my $numf = @{ $res->[2] };
 
-        $dbh->do("INSERT INTO package (name, summary, metadata, mtime) VALUES (?,?,?,0)", {}, $pkg, $pkgmeta->{summary}, $json->encode($pkgmeta), $st[9]) unless $rec;
+        $dbh->do("INSERT INTO package (name, summary, metadata, mtime) VALUES (?,?,?,0)", {}, $pkg, $pkgmeta->{summary}, _json->encode($pkgmeta), $st[9]) unless $rec;
         $dbh->do("UPDATE package set mtime=? WHERE name=?", {}, $st[9], $pkg);
         $dbh->do("DELETE FROM function WHERE package=?", {}, $pkg);
         my $j = 0;
@@ -283,10 +329,10 @@ sub update_rinci_metadata_db {
             $j++;
             $log->debug("Processing function $pkg\::$f ...");
             $progress->update(pos => $i + $j/$numf, message => "Processing function $pkg\::$f ...") if $progress;
-            $res = $pa->request(meta => "$uri$e");
+            $res = _pa->request(meta => "$uri$e");
             die "Can't meta $e: $res->[0] - $res->[1]" unless $res->[0] == 200;
-            $cleanser->clean_in_place(my $meta = $res->[2]);
-            $dbh->do("INSERT INTO function (package, name, summary, metadata) VALUES (?,?,?,?)", {}, $pkg, $f, $meta->{summary}, $json->encode($meta));
+            _cleanser->clean_in_place(my $meta = $res->[2]);
+            $dbh->do("INSERT INTO function (package, name, summary, metadata) VALUES (?,?,?,?)", {}, $pkg, $f, $meta->{summary}, _json->encode($meta));
         }
     }
     $progress->finish if $progress;
@@ -310,5 +356,221 @@ sub update_rinci_metadata_db {
     [200, "OK"];
 }
 
+$SPEC{update} = {
+    v => 1.1,
+    summary => 'Add/update a package or function metadata',
+    args => {
+        %args_common,
+        package => {
+            schema => 'str*',
+            req => 1,
+        },
+        function => {
+            schema => 'str*',
+        },
+        metadata => {
+            schema => 'hash*',
+            req => 1,
+        },
+        dist => {
+            schema => 'str*',
+        },
+        extra => {
+            schema => 'str*',
+        },
+    },
+};
+sub update {
+    require Perinci::Sub::Normalize;
+
+    my %args = @_;
+
+    my ($res, $dbh) = _connect_db(%args);
+    return $res unless $res->[0] == 200;
+
+    my $pkg  = $args{package};
+    my $func = $args{function};
+    my $meta = $args{metadata};
+
+    if ($func) {
+        $meta = Perinci::Sub::Normalize::normalize_function_metadata($meta);
+    }
+
+    my $pkgsummary;
+    $pkgsummary = $meta->{summary} unless $func;
+    if ($dbh->selectrow_array("SELECT name FROM package WHERE name=?", {}, $pkg)) {
+        $dbh->do("UPDATE package (summary, metadata, mtime, dist, extra) VALUES (?,?,?,?,?) WHERE package=?",
+                 {}, $pkgsummary, _json->encode($meta), time(), $args{dist}, $args{extra},
+                 $pkg);
+    } else {
+        $dbh->do("INSERT INTO package (name, summary, metadata, mtime, extra) VALUES (?,?,?,?,?)",
+                 {}, $pkg, $pkgsummary, _json->encode($meta), $args{dist}, $args{extra});
+    }
+
+    if ($func) {
+        my $funcsummary = $meta->{summary};
+        if ($dbh->selectrow_array("SELECT name FROM function WHERE package=? AND name=?", {}, $pkg, $func)) {
+            $dbh->do("UPDATE function (summary, metadata, mtime, extra) VALUES (?,?,?,?) WHERE package=? AND function=?",
+                     {}, $funcsummary, _json->encode($meta), time(), $args{extra},
+                     $pkg, $func);
+        } else {
+            $dbh->do("INSERT INTO package (package, name, summary, metadata, mtime, extra) VALUES (?,?,?,?,?,?)",
+                     {}, $pkg, $func, $funcsummary, _json->encode($meta), time(), $args{extra});
+        }
+    }
+
+    [200, "OK"];
+}
+
+$SPEC{delete} = {
+    v => 1.1,
+    summary => 'Delete a package or function metadata',
+    args => {
+        %args_common,
+        package => {
+            schema => 'str*',
+            req => 1,
+        },
+        function => {
+            schema => 'str*',
+        },
+    },
+};
+sub delete {
+    my %args = @_;
+
+    my ($res, $dbh) = _connect_db(%args);
+    return $res unless $res->[0] == 200;
+
+    my $pkg  = $args{package};
+    my $func = $args{function};
+
+    if ($func) {
+        $dbh->do("DELETE FROM function WHERE package=? AND name=?", {}, $pkg, $func);
+    } else {
+        $dbh->do("DELETE FROM function WHERE package=?", {}, $pkg);
+        $dbh->do("DELETE FROM package WHERE name=?", {}, $pkg);
+    }
+    [200, "OK"];
+}
+
+$SPEC{packages} = {
+    v => 1.1,
+    summary => 'List packages',
+    args => {
+        %args_common,
+        %args_query,
+    },
+};
+sub packages {
+    my %args = @_;
+
+    my ($res, $dbh) = _connect_db(%args);
+    return $res unless $res->[0] == 200;
+
+    my $q  = $args{query};
+
+    my @rows;
+    my @columns = qw(name summary dist mtime extra);
+    my @wheres;
+    my @binds;
+
+    if (length $q) {
+        push @wheres, "(name LIKE ? OR dist LIKE ? OR extra LIKE ?)";
+        push @binds, $q, $q, $q;
+    }
+
+    my $sth = $dbh->prepare(
+        "SELECT name,summary,dist,mtime,extra FROM package ORDER by name".
+            (@wheres ? " WHERE ".join(" AND ", @wheres) : "")
+    );
+    $sth->execute(@binds);
+
+    while (my $row = $sth->fetchrow_hashref) {
+        if ($args{detail}) {
+            push @rows, $row;
+        } else {
+            push @rows, $row->{name};
+        }
+    }
+
+    [200, "OK", \@rows, {'table.fields'=>\@columns}];
+}
+
+$SPEC{functions} = {
+    v => 1.1,
+    summary => 'List functions',
+    args => {
+        %args_common,
+        %args_query,
+    },
+};
+sub functions {
+    my %args = @_;
+
+    my ($res, $dbh) = _connect_db(%args);
+    return $res unless $res->[0] == 200;
+
+    my $q  = $args{query};
+
+    my @rows;
+    my @columns = qw(package name summary extra);
+    my @wheres;
+    my @binds;
+
+    if (length $q) {
+        push @wheres, "(package LIKE ? OR name LIKE ? OR extra LIKE ?)";
+        push @binds, $q, $q, $q;
+    }
+
+    my $sth = $dbh->prepare(
+        "SELECT package,name,summary,extra FROM function ORDER by package,name".
+            (@wheres ? " WHERE ".join(" AND ", @wheres) : "")
+    );
+    $sth->execute(@binds);
+
+    while (my $row = $sth->fetchrow_hashref) {
+        if ($args{detail}) {
+            push @rows, $row;
+        } else {
+            push @rows, $row->{name};
+        }
+    }
+
+    [200, "OK", \@rows, {'table.fields'=>\@columns}];
+}
+
+$SPEC{stats} = {
+    v => 1.1,
+    summary => 'Show some statistics',
+};
+sub stats {
+    my %args = @_;
+
+    my ($res, $dbh) = _connect_db(%args);
+    return $res unless $res->[0] == 200;
+
+    my %stats;
+    ($stats{num_packages}) = $dbh->selectrow_array("SELECT COUNT(*) FROM package");
+    ($stats{num_functions}) = $dbh->selectrow_array("SELECT COUNT(*) FROM function");
+
+    # XXX avg_num_args
+    # XXX top_arg_types
+    # XXX top_arg_entities
+    # XXX pct_arg_has_entity
+    # XXX pct_arg_has_element_entity
+
+    [200, "OK", \%stats];
+}
+
 1;
 # ABSTRACT:
+
+=head1 SYNOPSIS
+
+See the included CLI script L<rimetadb>.
+
+
+=head1 SEE ALSO
+
+L<Rinci>
